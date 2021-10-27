5c920ef5a5386651109abff320b5af22444d2150
longyue <184579801@qq.com>
2021-10-27 23:45:51 +0800

    inner angle

---
+++ Assets/CustomRP/Editor/CustomLightEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+using CustomSR;
+
+[CanEditMultipleObjects]
+[CustomEditorForRenderPipeline(typeof(Light), typeof(CustomRendePineAsset))]
+public class CustomLightEditor : LightEditor 
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if ( !settings.lightType.hasMultipleDifferentValues &&
+            (LightType)settings.lightType.enumValueIndex == LightType.Spot)
+        {
+            settings.DrawInnerAndOuterSpotAngle();
+            settings.ApplyModifiedProperties();
+        }
+    }
+}
